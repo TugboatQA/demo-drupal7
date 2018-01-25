37689cb0718b7d8c18562aa03caf41a665c34fb3
Ben Chavet <ben.chavet@lullabot.com>
2018-01-25 15:39:41 -0600

    Create drupal settings.php files

---
+++ docroot/sites/default/settings.tugboat.php
@@ -0,0 +1,10 @@
+<?php
+$databases = array(
+    'default' => array(
+        'driver' => 'mysql',
+        'database' => 'drupal7',
+        'username' => 'tugboat',
+        'password' => 'tugboat',
+        'host' => 'mysql',
+    )
+);
---
+++ docroot/sites/default/settings.vagrant.php
@@ -0,0 +1,16 @@
+<?php
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'drupal7',
+      'username' => 'drupal7',
+      'password' => 'drupal7',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
